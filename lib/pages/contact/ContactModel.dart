6d5c624dc15aadf7f762ac79f241fb5cc75a4f71
JuhoSong <qhdrhf5144@kaist.ac.kr>
2023-12-29 17:27:14 +0900

    viewContactpage_popup

--- lib/pages/contact/ContactModel.dart
+++ lib/pages/contact/ContactModel.dart
@@ -5,15 +5,17 @@ import 'package:flutter/material.dart';
 class Contact {
   final String name;
   final String phoneNumber;
+  final String relation;
 
 
-  Contact({required this.name, required this.phoneNumber});
+  Contact({required this.name, required this.phoneNumber, required this.relation});
 
 
   factory Contact.fromJson(Map<String, dynamic> json) {
     return Contact(
       name: json['name'],
       phoneNumber: json['phoneNumber'],
+      relation: json['relation'],
     );
   }
 }
--- lib/pages/contact/ViewContactPage.dart
+++ lib/pages/contact/ViewContactPage.dart
@@ -1,30 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:madcamp_1week/pages/contact/ContactModel.dart';
 
-
 class ViewContactPageWidget extends StatefulWidget {
   const ViewContactPageWidget({Key? key}) : super(key: key);
-
 
   @override
   State<ViewContactPageWidget> createState() => _ViewContactPageWidgetState();
 }
 
-
 class _ViewContactPageWidgetState extends State<ViewContactPageWidget> {
   final List<Contact> contacts = [
-    Contact(name: 'Juho Song', phoneNumber: '010-4055-5244'),
-    Contact(name: 'A', phoneNumber: '010-1111-1111'),
-    Contact(name: 'B', phoneNumber: '010-2222-2222'),
-    Contact(name: 'C', phoneNumber: '010-3333-3333'),
-    Contact(name: 'D', phoneNumber: '010-4444-4444'),
-    Contact(name: 'E', phoneNumber: '010-5555-5555'),
-    Contact(name: 'F', phoneNumber: '010-6666-6666'),
-    Contact(name: 'G', phoneNumber: '010-7777-7777'),
-    Contact(name: 'H', phoneNumber: '010-8888-8888'),
-    Contact(name: 'I', phoneNumber: '010-9999-9999'),
+    Contact(name: 'Alpha', phoneNumber: '010-1111-1111', relation: 'family'),
+    Contact(name: 'Bravo', phoneNumber: '010-2222-2222', relation: 'friend'),
+    Contact(name: 'Charlie', phoneNumber: '010-3333-3333', relation: 'relative'),
+    Contact(name: 'Delta', phoneNumber: '010-4444-4444', relation: 'family'),
+    Contact(name: 'Echo', phoneNumber: '010-5555-5555', relation: 'friend'),
+    Contact(name: 'Foxtrot', phoneNumber: '010-6666-6666', relation: 'friend'),
+    Contact(name: 'Golf', phoneNumber: '010-7777-7777', relation: 'family'),
+    Contact(name: 'Hotel', phoneNumber: '010-8888-8888', relation: 'friend'),
+    Contact(name: 'India', phoneNumber: '010-9999-9999', relation: 'relative'),
   ];
 
+void _showContactDetails(BuildContext context, Contact contact) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Container(
+          padding: EdgeInsets.only(top: 30.0),
+          child: Icon(Icons.person, size: 50.0, color: Colors.blue),
+        ),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: 20.0),
+            Text(
+              '${contact.name}',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '${contact.phoneNumber}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            Text(
+              '${contact.relation}',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ],
+        ),
+        contentPadding: EdgeInsets.only(top: 30.0),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.close, size: 20.0),
+            ), 
+          ),
+        ],
+      );
+    },
+  );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -33,16 +73,41 @@ class _ViewContactPageWidgetState extends State<ViewContactPageWidget> {
         title: Text('연락처'),
       ),
       body: ListView.builder(
-        itemCount: contacts.length,
+        itemCount: contacts.length * 2 - 1, // 선을 넣기 위해 항목 수 조정
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(contacts[index].name),
-            subtitle: Text(contacts[index].phoneNumber),
-          );
-        }
-      )
-
-
+          if (index.isOdd) {
+            // 홀수 인덱스에 Divider 추가
+            return Divider();
+          } else {
+            // 짝수 인덱스에 ListTile 추가
+            final contactIndex = index ~/ 2;
+            return ListTile(
+              title: RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: contacts[contactIndex].name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold, 
+                        fontSize: 20.0, // 이름의 폰트 사이즈
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' ${contacts[contactIndex].phoneNumber}',
+                      style: TextStyle(
+                        fontSize: 18.0, // 전화번호의 폰트 사이즈
+                        color: Colors.grey, // 선택사항: 전화번호에 대한 색상
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () => _showContactDetails(context, contacts[contactIndex]),
+            );
+          }
+        },
+      ),
     );
   }
 }
