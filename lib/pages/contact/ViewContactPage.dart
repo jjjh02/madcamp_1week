3e43d1cc445ebbf9d852693b3135e9bc3f486b15
JuhoSong <qhdrhf5144@kaist.ac.kr>
2024-01-01 19:01:02 +0900

    contact path_provider 수정

--- lib/pages/contact/ViewContactPage.dart
+++ lib/pages/contact/ViewContactPage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:madcamp_1week/pages/contact/ContactModel.dart';
-import 'package:contacts_service/contacts_service.dart';
-import 'package:permission_handler/permission_handler.dart';
-
+import 'dart:convert';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class ViewContactPageWidget extends StatefulWidget {
   final String peopleName;
@@ -16,17 +16,53 @@ class ViewContactPageWidget extends StatefulWidget {
 
 class _ViewContactPageWidgetState extends State<ViewContactPageWidget> {
 
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> _localFile() async {
+    final path = await _localPath;
+    return File('$path/contacts.json');
+  }
+
+  Future<void> _writeContacts() async {
+    final file = await _localFile();
+    final encodedData = jsonEncode(contacts);
+    await file.writeAsString(encodedData);
+  }
+
+  Future<List<ContactPeople>> _loadContacts() async {
+    try {
+      final file = await _localFile();
+      final contents = await file.readAsString();
+      final decodedData = jsonDecode(contents) as List<dynamic>;
+      return decodedData.map((json) => ContactPeople.fromJson(json)).toList();
+    } catch (e) {
+      // Handle errors or return an empty list
+      return [];
+    }
+  }
+
  @override
   void initState() {
     super.initState();
+
+    // Load contacts from file
+    _loadContacts().then((loadedContacts) {
+      setState(() {
+        contacts = loadedContacts;
+      });
+    });
+
     if (widget.peopleName.isNotEmpty) {
       // peopleName에 해당하는 연락처 찾기 (null 허용)
       final ContactPeople? contact = contacts.firstWhere(
         (contact) => contact.name == widget.peopleName,
         orElse: () {
-    // 기본 Contact 객체 반환 또는 다른 처리
-    return ContactPeople(name: 'Unknown', phoneNumber: '', relation: '');
-  }, // null 반환
+        // 기본 Contact 객체 반환 또는 다른 처리
+        return ContactPeople(name: 'Unknown', phoneNumber: '', relation: '');
+        }, // null 반환
       );
       if (contact != null) {
         WidgetsBinding.instance.addPostFrameCallback((_) => _showContactDetails(context, contact));
@@ -40,48 +76,95 @@ void _showContactDetails(BuildContext context, ContactPeople contact) {
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-  titlePadding: EdgeInsets.all(0),
-  title: Stack(
-    alignment: Alignment.topRight,
-    children: [
-      Container(
-        padding: EdgeInsets.only(top: 30.0),
-        alignment: Alignment.center,
-        child: Icon(Icons.person, size: 50.0, color: Color.fromARGB(255, 13, 114, 208)),
+      titlePadding: EdgeInsets.all(0),
+      title: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 30.0),
+            alignment: Alignment.center,
+            child: Icon(Icons.person, size: 50.0, color: Color.fromARGB(255, 13, 114, 208)),
+          ),
+          /*IconButton(
+            padding: EdgeInsets.only(top: 10.0, right: 10.0),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Icon(Icons.close, size: 20.0),
+          ),*/          
+        ],
       ),
-      IconButton(
-        padding: EdgeInsets.only(top: 10.0, right: 10.0),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        icon: Icon(Icons.close, size: 20.0),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SizedBox(height: 20.0),
+          Text(
+            '${contact.name}',
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '${contact.phoneNumber}',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            '${contact.relation}',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ],
       ),
-    ],
-  ),
-  content: Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      SizedBox(height: 20.0),
-      Text(
-        '${contact.name}',
-        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-      ),
-      Text(
-        '${contact.phoneNumber}',
-        style: TextStyle(fontSize: 18.0),
-      ),
-      Text(
-        '${contact.relation}',
-        style: TextStyle(fontSize: 18.0),
-      ),
-    ],
-  ),
-  contentPadding: EdgeInsets.only(top: 30.0, left: 24.0, right: 24.0, bottom: 24.0),
-);
+      actions: <Widget>[
+          ElevatedButton(
+            onPressed: () {
+              _deleteContact(contact);
+              Navigator.of(context).pop();
+            },
+            child: Text('삭제'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('닫기'),
+          ),
+        ],      
+      contentPadding: EdgeInsets.only(top: 30.0, left: 24.0, right: 24.0, bottom: 24.0),
+    );
     },
   );
 }
+
+void _addOrUpdateContact(String name, String phoneNumber, String relation) {
+  final newContact = ContactPeople(name: name, phoneNumber: phoneNumber, relation: relation);
+
+  // Assuming contacts is a list that contains all the contacts
+  contacts.add(newContact);
+
+  // Add the contact to the UI immediately
+  setState(() {});
+
+  // Save contacts to file
+  _writeContacts();
+
+  // Clear the text fields after adding the contact
+  nameController.clear();
+  phoneNumberController.clear();
+  relationController.clear();
+}
+
+void _deleteContact(ContactPeople contact) {
+  contacts.remove(contact);
+  
+  setState(() {});
+  
+  // Save contacts to file after deletion
+  _writeContacts();
+}
+
+  //String? _selectedRelation;
+  final nameController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final relationController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -125,6 +208,54 @@ void _showContactDetails(BuildContext context, ContactPeople contact) {
           }
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        //final whoController = TextEditingController();
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text("전화번호 추가"),
+            content: Column(
+              children: [
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(labelText: "이름"),
+                ),
+                TextField(
+                  controller: phoneNumberController,
+                  decoration: InputDecoration(labelText: "전화번호"),
+                ),
+                TextField(
+                  controller: relationController,
+                  decoration: InputDecoration(labelText: "관계"),
+                ),
+                ]),
+            actions: <Widget>[
+              TextButton(
+                child: Text("추가"),
+                onPressed: () {
+                  if (nameController.text.isNotEmpty &&
+                      phoneNumberController.text.isNotEmpty &&
+                      relationController.text.isNotEmpty) {
+                    _addOrUpdateContact(
+                      nameController.text,
+                      phoneNumberController.text,
+                      relationController.text,
+                    );
+                    Navigator.of(context).pop();
+                  }
+                },
+              ),
+              TextButton(
+                child: Text("취소"),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      child: Icon(Icons.add),
+    ),
     );
   }
 }
